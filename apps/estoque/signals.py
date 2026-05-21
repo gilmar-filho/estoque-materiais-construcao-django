b73dcf826857992eee4e784dacfc1a906ea919c6
gilmar-filho <gfilho416@gmail.com>
2026-05-21 09:04:55 -0300

    fix(estoque): correção na lógica de atualização de quantidade_física na tabela Produto quando atualização é feita diretamente em MovimentacaoEstoque

--- apps/estoque/signals.py
+++ apps/estoque/signals.py
@@ -1,5 +1,6 @@
 from django.db.models.signals import post_save, post_delete
 from django.dispatch import receiver
+from apps.estoque.models import MovimentacaoEstoque
 
 @receiver(post_save, sender='vitrine.ItemReserva')
 def adicionar_reserva(sender, instance, created, **kwargs):
@@ -11,3 +12,17 @@ def adicionar_reserva(sender, instance, created, **kwargs):
 def remover_reserva(sender, instance, **kwargs):
     instance.produto.quantidade_reservada -= instance.quantidade
     instance.produto.save()
+
+@receiver(post_save, sender=MovimentacaoEstoque)
+def atualizar_estoque_fisico(sender, instance, created, **kwargs):
+    if not created:
+        return
+    
+    produto = instance.produto
+    
+    if instance.tipo == 'ENTRADA':
+        produto.quantidade_fisica += instance.quantidade
+    elif instance.tipo in ('SAIDA', 'AJUSTE_PERDA'):
+        produto.quantidade_fisica -= instance.quantidade
+    
+    produto.save()
