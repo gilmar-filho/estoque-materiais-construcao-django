3051d272667108d998e6ba380dd72b7db83ecf62
JoaoMarcus12 <silvajoaomarcus8@gmail.com>
2026-05-30 16:49:34 -0300

    Adição do Tailwhind e estilização da vitrine

--- apps/vitrine/forms.py
+++ apps/vitrine/forms.py
@@ -1,6 +1,5 @@
 from django import forms
 from django.contrib.auth.forms import UserCreationForm
-
 from apps.core.models import Usuario
 
 
@@ -8,10 +7,37 @@ class CadastroClienteForm(UserCreationForm):
 
     class Meta:
         model = Usuario
+
         fields = [
             'username',
             'email',
             'telefone',
             'password1',
-            'password2',
+            'password2'
         ]
+
+        widgets = {
+            'username': forms.TextInput(attrs={
+                'class': 'w-full px-4 py-3 border border-gray-300 rounded-lg'
+            }),
+
+            'email': forms.EmailInput(attrs={
+                'class': 'w-full px-4 py-3 border border-gray-300 rounded-lg'
+            }),
+
+            'telefone': forms.TextInput(attrs={
+                'class': 'w-full px-4 py-3 border border-gray-300 rounded-lg'
+            }),
+        }
+
+    password1 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'w-full px-4 py-3 border border-gray-300 rounded-lg'
+        })
+    )
+
+    password2 = forms.CharField(
+        widget=forms.PasswordInput(attrs={
+            'class': 'w-full px-4 py-3 border border-gray-300 rounded-lg'
+        })
+    )
